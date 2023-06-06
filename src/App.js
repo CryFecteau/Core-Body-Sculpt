d2965451cd6ce74895af535ee9ba951f9d0bb7df
Cry <cry.fecteau@gmail.com>
2023-06-05 17:32:37 -0700

    Base styling for programs section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HeroComponent from './components/hero-component/hero.component';
+import ProgramsComponent from './components/programs-component/programs-component';
 
 function App() {
   return (
     <>
       <HeroComponent />
+      <ProgramsComponent />
     </>
   );
 }
---
+++ src/components/programs-component/programs-component.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './programs-component.css'
+
+export default function ProgramsComponent() {
+    return (
+        <div id='programs-container' className="programs-container">
+            <div className="programs-header-container">
+                <span className='app-stroke-text'>Unlock Your</span>
+                <span>Fitness</span>
+                <span className='app-stroke-text'>Goals With Us</span>
+            </div>
+            <div className="programs-categories-container">
+                <div className="programs-category">
+                    <img className='category-icon' src="https://via.placeholder.com/50" alt="program-img" />
+                    <span className='category-heading'>Heading Here</span>
+                    <span className='category-detail'>Details: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab, deserunt repudiandae, sequi vero id sint molestias a expedita dignissimos placeat quod, officiis voluptate! Est at dolorum blanditiis autem? Aperiam, optio?</span>
+                    <div className="join-now-btn">
+                        <span>Join Now</span>
+                        <img src="https://via.placeholder.com/50" alt="program-img" />
+                    </div>
+                </div>
+                <div className="programs-category">
+                    <img className='category-icon' src="https://via.placeholder.com/50" alt="program-img" />
+                    <span className='category-heading'>Heading Here</span>
+                    <span className='category-detail'>Details: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab, deserunt repudiandae, sequi vero id sint molestias a expedita dignissimos placeat quod, officiis voluptate! Est at dolorum blanditiis autem? Aperiam, optio?</span>
+                    <div className="join-now-btn">
+                        <span>Join Now</span>
+                        <img src="https://via.placeholder.com/50" alt="program-img" />
+                    </div>
+                </div>
+                <div className="programs-category">
+                    <img className='category-icon' src="https://via.placeholder.com/50" alt="program-img" />
+                    <span className='category-heading'>Heading Here</span>
+                    <span className='category-detail'>Details: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab, deserunt repudiandae, sequi vero id sint molestias a expedita dignissimos placeat quod, officiis voluptate! Est at dolorum blanditiis autem? Aperiam, optio?</span>
+                    <div className="join-now-btn">
+                        <span>Join Now</span>
+                        <img src="https://via.placeholder.com/50" alt="program-img" />
+                    </div>
+                </div>
+                <div className="programs-category">
+                    <img className='category-icon' src="https://via.placeholder.com/50" alt="program-img" />
+                    <span className='category-heading'>Heading Here</span>
+                    <span className='category-detail'>Details: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab, deserunt repudiandae, sequi vero id sint molestias a expedita dignissimos placeat quod, officiis voluptate! Est at dolorum blanditiis autem? Aperiam, optio?</span>
+                    <div className="join-now-btn">
+                        <span>Join Now</span>
+                        <img src="https://via.placeholder.com/50" alt="program-img" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
